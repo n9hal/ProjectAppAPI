5a78ba4814e36dd6032d0468ec8a2e137f8b120b
Nehal Patel <n9hal09@hotmail.com>
2021-08-14 18:17:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import numpy as np
 from flask import  Flask, request,render_template,jsonify,url_for
 from flask_cors import CORS 
 import os
-import pickle
+import pickle5 as pickle
 import flask
 from newspaper import Article
 import urllib
@@ -11,9 +11,8 @@ app= Flask(__name__)
 CORS(app)
 app= flask.Flask(__name__,template_folder='templates')
 
-path="D:\Codes\Python_Codes\env\model_modified.pkl"
-with open(path,'rb') as handle:
-    model=pickle.load(handle)
+pickle_in=open('model_modified','rb')
+model=pickle.load(pickle_in)
 
 @app.route('/')
 def main():
