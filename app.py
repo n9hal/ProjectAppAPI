2c4cda879ba6eafe74fbbdfaf40bd33a7baaf09e
Nehal Patel <n9hal09@hotmail.com>
2021-08-14 18:19:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -56,5 +56,4 @@ def checknews():
     return render_template('result.html'.format(result))
 
 if __name__=="__main__":
-    port =int(os.environ.get('PORT',5000))
     app.run(host='0.0.0.0',port=port,debug=True)
